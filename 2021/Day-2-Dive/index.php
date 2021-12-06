ad31306e217ebfd10dd3d070d4326453c1610efa
Sebas Korko <frozedviking@gmail.com>
2021-12-06 03:59:44 -0300

    Problem solved :D
    Cast the $number into (int)
    Check the command and do the appropiate operation, acording to the excersice.
    Print values and text
    Get the solution by multiplication
    Print the solution

--- 2021/Day-2-Dive/index.php
+++ 2021/Day-2-Dive/index.php
@@ -7,7 +7,7 @@
 * By TheKorko
 * https://github.com/thekorko
 * https://quartex.net/
-* count the number of times a depth measurement increases from the previous measurement.
+* Learn to operate the submarine, using the input commands, then obtain the solution doing a multiplication of depth and horizontal position
 *
     forward X increases the horizontal position by X units.
     down X increases the depth by X units.
@@ -19,31 +19,32 @@ $commandList = file('input', FILE_IGNORE_NEW_LINES);
 $counter = 0;
 $horizontalPos = 0;
 $depth = 0;
-$auxNumber = 0;
 foreach ($commandList as $commandStr) {
+  //Our command are only lowercase, we want to remove spaces and the number
   $command = preg_replace('/[^a-z]/', '', $commandStr);
-  $number = filter_var($commandStr, FILTER_SANITIZE_NUMBER_INT);
-  var_dump($commandStr);
-  echo "The command is $command and the number is $number<br>";
-  /*
-  if ($command==0) {
-    echo "$number (N/A - no previous measurement)<br>";
-  } elseif ($command>$auxNumber) {
-    echo "$number (increased)<br>";
-    $howManyLargerDepths++;
-  } elseif ($command<$auxNumber) {
-    echo "$number (decreased)<br>";
+  //Our number is the quantity which will be added or substracted, so only an integer without spaces
+  $number = (int)filter_var($commandStr, FILTER_SANITIZE_NUMBER_INT);
+  //var_dump($commandStr);
+  //var_dump($number);
+  if ($command=='forward') {
+    $horizontalPos += $number;
+    echo "The command is $command, it <b>increases</b> the <b>horizontal position</b> by $number units, a total of $horizontalPos.<br>";
+  } elseif ($command=='down') {
+    $depth += $number;
+    echo "The command is $command, it <b>increases</b> the <b>depth</b> by $number units, a total of $depth.<br>";
+  } elseif ($command=='up') {
+    $depth -= $number;
+    echo "The command is $command, it <b>decreases</b> the <b>depth</b> by $number units, a total of $depth.<br>";
   } else {
-    echo "$number (equal)<br>";
+    echo "Command still not supported.";
   }
-  $auxNumber = $number;
   $counter++;
-  */
 }
-echo "In this example, there are  measurements that are larger than the previous measurement.";
+$solution = $depth*$horizontalPos;
+echo "<br><p>In this example, there were $counter iterations<br> After these instructions, you would have a horizontal position of $horizontalPos and a depth of $depth. (Multiplying these together produces $solution.)</p>";
 
 //What do you get if you multiply your final horizontal position by your final depth?
-//
-//
+//My answer was 1580000
+//That's the right answer! You are one gold star closer to finding the sleigh keys.
 
 ?>
